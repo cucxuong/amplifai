ecd8f16dfb848fbb686c6ae7fd4e92a986d565ee
cucxuong <61396686+cucxuong@users.noreply.github.com>
2026-05-19 15:17:20 +0700

    apply view-transition

--- app/pages/sign-in/index.vue
+++ app/pages/sign-in/index.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+definePageMeta({
+  viewTransition: {
+    enabled: true,
+    types: ['fade'],
+    toTypes: ['slide-in'],
+    fromTypes: ['slide-out'],
+  },
+})
+</script>
+
 <template>
   <PageSignIn />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,6 +44,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  experimental: {
+    viewTransition: true,
+  },
+
   vite: {
     plugins: [
       tailwindcss(),
